9bb40e3708ce163618a36e717ed6702e1e07ff48
Lovanirina R <lovanirina.ran@gmail.com>
2025-01-04 19:56:35 +0300

    feat: add GraphQL client utility with authentication for GitHub API

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,22 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { graphql } from "@octokit/graphql";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+/**
+ * Returns a GraphQL client instance with authentication headers set for GitHub GraphQL API.
+ *
+ * @param githubToken - The GitHub token to be used for authentication.
+ * @returns A GraphQL client instance with the authorization header set.
+ * @remarks The GitHub GraphQL API endpoint is https://api.github.com/graphql.
+ */
+export function getGraphqlWithAuth(githubToken: string) {
+  return graphql.defaults({
+    headers: {
+      authorization: `token ${githubToken}`,
+    },
+  });
 }
