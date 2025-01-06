8a13916005ee251b3a581ef00b2276bced2267ac
Lovanirina R <lovanirina.ran@gmail.com>
2025-01-06 04:52:17 +0300

    feat: add RankCard and RankNumber components for ranking display

---
+++ components/RankCard.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import type { RankUser } from "~/types";
+
+const { title, users, rankKey } = defineProps<{
+  title: string;
+  users: RankUser[];
+  rankKey: keyof RankUser;
+}>();
+</script>
+
+<template>
+  <Card class="w-full">
+    <CardHeader>
+      <CardTitle class="text-lg">{{ title }}</CardTitle>
+    </CardHeader>
+    <CardContent class="text-xs">
+      <ul class="grid gap-2">
+        <li
+          v-for="(user, index) in users"
+          :key="user.login"
+          class="flex items-center"
+        >
+          <RankNumber :number="index + 1" class="" />
+          <Avatar class="ml-1 size-8">
+            <AvatarImage :src="user.avatarUrl" />
+            <AvatarFallback>{{ user.login }}</AvatarFallback>
+          </Avatar>
+          <div class="ml-2 flex-1 font-semibold">{{ user.login }}</div>
+          <div class="inline-flex">{{ user[rankKey] }}</div>
+        </li>
+      </ul>
+    </CardContent>
+  </Card>
+</template>
---
+++ components/RankNumber.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from "vue";
+import { cn } from "~/lib/utils";
+
+const top1 = "border-2 border-yellow-500 bg-yellow-300 text-yellow-700";
+const top2 = "border-2 border-gray-500 bg-gray-300 text-gray-700";
+const top3 = "border-2 border-yellow-800 bg-yellow-600 text-yellow-950";
+
+const props = defineProps<{
+  number: number;
+  class?: HTMLAttributes["class"];
+}>();
+</script>
+
+<template>
+  <div
+    :class="
+      cn(
+        'text-centers flex aspect-square size-6 items-center justify-center rounded-full text-xs font-semibold',
+        props.number === 1 ? top1 : '',
+        props.number === 2 ? top2 : '',
+        props.number === 3 ? top3 : '',
+        props.class,
+      )
+    "
+  >
+    {{ props.number }}
+  </div>
+</template>
+
+<style scoped></style>
