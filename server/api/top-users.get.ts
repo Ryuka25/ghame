9816ac129d881023916ba5817141fc6c1f1ab0c7
Lovanirina R <lovanirina.ran@gmail.com>
2025-01-06 04:46:56 +0300

    refactor: replace User interface with RankUser type for clarity

--- server/api/top-users.get.ts
+++ server/api/top-users.get.ts
@@ -1,46 +1,38 @@
 import type { H3Event } from "h3";
-
-interface User {
-  login: string;
-  name: string;
-  avatar: string;
-  location: string;
-  company: string;
-  twitterUsername: string;
-  followers: number;
-  privateContributions: number;
-  publicContributions: number;
-}
+import type { RankUser } from "~/types";
 
 const TOP_MADAGASCAR_GITHUB_USERS =
   "https://raw.githubusercontent.com/tsirysndr/top-github-users/refs/heads/main/cache/madagascar.json";
 
-const getTopUsersByFollowers = (users: Array<User>) => {
+const getTopUsersByFollowers = (users: RankUser[]) => {
   users.sort((a, b) => b.followers - a.followers);
   return users.slice(0, 10);
 };
 
-const getTopUsersByPublicContributions = (users: Array<User>) => {
+const getTopUsersByPublicContributions = (users: RankUser[]) => {
   users.sort((a, b) => b.publicContributions - a.publicContributions);
   return users.slice(0, 10);
 };
 
-const getTopUsersByTotalContributions = (users: Array<User>) => {
+const getTopUsersByTotalContributions = (users: RankUser[]) => {
   users.sort((a, b) => {
-    const bTotalContributions = b.publicContributions + b.privateContributions;
-    const aTotalContributions = a.publicContributions + a.privateContributions;
-    return bTotalContributions - aTotalContributions;
+    return b.totalContributions - a.totalContributions;
   });
   return users.slice(0, 10);
 };
 
 export default defineEventHandler(async (event: H3Event) => {
   const usersJson = await $fetch<string>(TOP_MADAGASCAR_GITHUB_USERS);
-  const users: Array<User> = JSON.parse(usersJson);
+  const users: RankUser[] = JSON.parse(usersJson);
+  users.map(
+    (user) =>
+      (user.totalContributions =
+        user.publicContributions + user.privateContributions),
+  );
 
   return {
     followers: getTopUsersByFollowers(users),
-    "total-contributions": getTopUsersByTotalContributions(users),
-    "public-contributions": getTopUsersByPublicContributions(users),
+    total_contributions: getTopUsersByTotalContributions(users),
+    public_contributions: getTopUsersByPublicContributions(users),
   };
 });
