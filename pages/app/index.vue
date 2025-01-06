deb470f22a63fa0610d438be66c7c71c3a5b2e6d
Lovanirina R <lovanirina.ran@gmail.com>
2025-01-06 04:53:05 +0300

    feat(app): add top user rankings and loading indicator

--- pages/app/index.vue
+++ pages/app/index.vue
@@ -1,5 +1,21 @@
 <script lang="ts" setup>
+import { Loader2 } from "lucide-vue-next";
+import type { RankUser } from "~/types";
+
 const user = useUser();
+
+const topUsersByFollowers = useState<RankUser[]>(
+  "topUsersByFollowers",
+  () => [],
+);
+const topUsersByPublicContributions = useState<RankUser[]>(
+  "topUsersByPublicContributions",
+  () => [],
+);
+const topUsersByTotalContributions = useState<RankUser[]>(
+  "topUsersByTotalContributions",
+  () => [],
+);
 
 definePageMeta({
   middleware: "auth",
@@ -11,14 +27,40 @@ onMounted(() => {
       user.value = userData;
     });
   }
+
+  $fetch("/api/top-users").then((data) => {
+    topUsersByFollowers.value = data.followers;
+    topUsersByTotalContributions.value = data.total_contributions;
+    topUsersByPublicContributions.value = data.public_contributions;
+  });
 });
 </script>
 
 <template>
-  <section
-    class="mx-auto flex h-full max-w-7xl flex-col items-center justify-center px-4 py-4 sm:px-6 lg:flex-row lg:px-8"
-  >
-    <ProfileCard v-if="user" :user-profile="user" class="w-full max-w-sm" />
-    <div v-else>Waiting for user data...</div>
+  <section class="mx-auto h-full max-w-7xl px-4 py-4 sm:px-6 lg:px-8">
+    <div v-if="!user" class="flex h-full items-center justify-center">
+      <Loader2 class="animate-spin" />
+    </div>
+
+    <div v-else class="flex flex-col items-center gap-8">
+      <ProfileCard :user-profile="user" class="w-full max-w-xs" />
+      <div class="flex w-full flex-col gap-4">
+        <RankCard
+          :users="topUsersByPublicContributions"
+          rank-key="publicContributions"
+          title="Public contributions"
+        />
+        <RankCard
+          :users="topUsersByTotalContributions"
+          rank-key="totalContributions"
+          title="Total contributions"
+        />
+        <RankCard
+          :users="topUsersByFollowers"
+          rank-key="followers"
+          title="Followers"
+        />
+      </div>
+    </div>
   </section>
 </template>
