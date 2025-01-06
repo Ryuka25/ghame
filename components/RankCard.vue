638979a288fb69c123b59ec889f3c9d23438c223
Lovanirina R <lovanirina.ran@gmail.com>
2025-01-06 05:37:08 +0300

    fix(rank-card): adjust avatar and text margin spacing

--- components/RankCard.vue
+++ components/RankCard.vue
@@ -21,11 +21,11 @@ const { title, users, rankKey } = defineProps<{
           class="flex items-center"
         >
           <RankNumber :number="index + 1" class="" />
-          <Avatar class="ml-1 size-8">
+          <Avatar class="ml-2 size-8">
             <AvatarImage :src="user.avatarUrl" />
             <AvatarFallback>{{ user.login }}</AvatarFallback>
           </Avatar>
-          <div class="ml-2 flex-1 font-semibold">{{ user.login }}</div>
+          <div class="ml-2 flex-1 font-semibold md:ml-4">{{ user.login }}</div>
           <div class="inline-flex">{{ user[rankKey] }}</div>
         </li>
       </ul>
