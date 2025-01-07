d8f29b2c144e9a5af3b3284d0fe18d7f413411ad
Lovanirina R <lovanirina.ran@gmail.com>
2025-01-07 05:50:08 +0300

    refactor: use RankCardItem component for user ranking list

--- components/RankCard.vue
+++ components/RankCard.vue
@@ -19,22 +19,14 @@ const { title, users, rankKey } = defineProps<{
         <span>{{ title }}</span>
       </CardTitle>
     </CardHeader>
-    <CardContent class="text-xs">
-      <ul class="grid gap-2">
-        <li
-          v-for="(user, index) in users"
-          :key="user.login"
-          class="flex items-center"
-        >
-          <RankNumber :number="index + 1" class="" />
-          <Avatar class="ml-2 size-8">
-            <AvatarImage :src="user.avatarUrl" />
-            <AvatarFallback>{{ user.login }}</AvatarFallback>
-          </Avatar>
-          <div class="ml-2 flex-1 font-semibold md:ml-4">{{ user.login }}</div>
-          <div class="inline-flex">{{ user[rankKey] }}</div>
-        </li>
-      </ul>
+    <CardContent class="grid gap-2 text-xs">
+      <RankCardItem
+        v-for="(user, index) in users"
+        :key="user.login"
+        :rank="index + 1"
+        :rank-key="rankKey"
+        :user="user"
+      />
     </CardContent>
   </Card>
 </template>
