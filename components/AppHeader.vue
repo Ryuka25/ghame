b549532e5f8701346d1b48228b20df4382206c0a
Lovanirina R <lovanirina.ran@gmail.com>
2025-01-08 19:35:29 +0300

    feat: improve navigation ui

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,15 @@
 <script lang="ts" setup>
 import { ContactRound, Menu } from "lucide-vue-next";
 import { APP_GITHUB_LINK } from "~/lib/app";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "~/components/ui/navigation-menu";
+
+const router = useRouter();
 
 const navigation = [
   {
@@ -45,19 +54,31 @@ const closeMenu = () => {
             </Button>
           </SheetTrigger>
           <SheetContent side="left">
-            <NuxtLink class="flex-1" to="/" @click="closeMenu">
-              <AppName />
-            </NuxtLink>
-            <nav class="w-full bg-background">
+            <div class="mt-4 flex items-center gap-4">
+              <NuxtLink class="flex-1" to="/" @click="closeMenu">
+                <AppName />
+              </NuxtLink>
+              <div class="my-2 flex gap-2">
+                <AppToogleModeButton show-label />
+                <AppGithubButton
+                  v-if="APP_GITHUB_LINK"
+                  :link="APP_GITHUB_LINK"
+                  show-label
+                />
+              </div>
+            </div>
+            <Separator />
+
+            <nav class="w-full">
               <ul class="flex flex-col justify-center gap-4 p-4">
                 <li v-for="link in navigation" :key="link.to">
                   <NuxtLink
                     :to="link.to"
                     active-class="!text-foreground"
-                    class="flex items-center gap-2 text-foreground/80 hover:text-foreground"
+                    class="flex h-8 items-center gap-2 text-sm text-foreground/80 hover:text-foreground"
                     @click="closeMenu"
                   >
-                    <component :is="link.icon" class="size-5" />
+                    <component :is="link.icon" class="size-4" />
                     {{ link.label }}
                   </NuxtLink>
                 </li>
@@ -69,30 +90,41 @@ const closeMenu = () => {
       <!-- /Mobile navigation -->
 
       <!--      App logo -->
-      <NuxtLink class="flex-1" to="/">
+      <NuxtLink class="flex flex-1 justify-center" to="/">
         <AppName />
       </NuxtLink>
       <!--      /App logo -->
 
       <!-- Desktop Navigation -->
-      <nav class="hidden lg:block">
-        <ul class="flex gap-4">
-          <li v-for="link in navigation" :key="link.to">
-            <NuxtLink
-              :to="link.to"
-              active-class="!text-foreground"
-              class="text-foreground/60 opacity-80 transition-colors hover:text-foreground/80"
+      <NavigationMenu class="hidden lg:block">
+        <NavigationMenuList>
+          <NavigationMenuItem
+            v-for="link in navigation"
+            :key="link.to"
+            class="w-full"
+          >
+            <NavigationMenuLink
+              :class="[
+                navigationMenuTriggerStyle(),
+                'w-full gap-2',
+                router.currentRoute.value.path == link.to ? 'text-primary' : '',
+              ]"
+              :href="link.to"
             >
               {{ link.label }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </nav>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
       <!-- /Desktop Navigation -->
       <div class="flex flex-1 items-center justify-end gap-1">
-        <AppToogleModeButton />
-        <AppGithubButton v-if="APP_GITHUB_LINK" :link="APP_GITHUB_LINK" />
-        <UserNav v-if="user" :user />
+        <AppToogleModeButton class="hidden lg:inline-flex" />
+        <AppGithubButton
+          v-if="APP_GITHUB_LINK"
+          :link="APP_GITHUB_LINK"
+          class="hidden lg:inline-flex"
+        />
+        <UserNav :user />
       </div>
     </div>
   </header>
--- components/UserNav.vue
+++ components/UserNav.vue
@@ -8,37 +8,53 @@ const { user } = defineProps<{
 </script>
 
 <template>
-  <DropdownMenu>
-    <DropdownMenuTrigger as-child>
-      <Button class="relative h-8 w-8 rounded-full" variant="ghost">
-        <Avatar class="h-8 w-8">
-          <AvatarImage :alt="user.login" :src="user.avatarUrl" />
-          <AvatarFallback>{{ user.initial }}</AvatarFallback>
-        </Avatar>
-      </Button>
-    </DropdownMenuTrigger>
-    <DropdownMenuContent align="end" class="w-56">
-      <DropdownMenuLabel class="flex font-normal">
-        <div class="flex flex-col space-y-1">
-          <p class="text-sm font-medium leading-none">{{ user.login }}</p>
-          <p
-            v-if="user.name"
-            class="text-xs leading-none text-muted-foreground"
+  <template v-if="user">
+    <DropdownMenu>
+      <DropdownMenuTrigger as-child>
+        <Button class="relative h-8 w-8 rounded-full" variant="ghost">
+          <Avatar class="h-8 w-8">
+            <AvatarImage :alt="user.login" :src="user.avatarUrl" />
+            <AvatarFallback>{{ user.initial }}</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" class="w-56">
+        <DropdownMenuLabel class="flex font-normal">
+          <div class="flex flex-col space-y-1">
+            <p class="text-sm font-medium leading-none">{{ user.login }}</p>
+            <p
+              v-if="user.name"
+              class="text-xs leading-none text-muted-foreground"
+            >
+              {{ user.name }}
+            </p>
+            <p v-else class="text-xs leading-none text-muted-foreground">
+              {{ user.location }}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem as-child>
+          <NuxtLink
+            :to="{
+              path: '/logout',
+              query: { next: router.currentRoute.value.path },
+            }"
           >
-            {{ user.name }}
-          </p>
-          <p v-else class="text-xs leading-none text-muted-foreground">
-            {{ user.location }}
-          </p>
-        </div>
-      </DropdownMenuLabel>
-      <DropdownMenuSeparator />
-      <DropdownMenuItem as-child>
-        <NuxtLink to="/logout/">
-          <LogOut class="mr-2 h-4 w-4" />
-          <span>Log out</span>
-        </NuxtLink>
-      </DropdownMenuItem>
-    </DropdownMenuContent>
-  </DropdownMenu>
+            <LogOut class="mr-2 h-4 w-4" />
+            <span>Log out</span>
+          </NuxtLink>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </template>
+  <template v-else>
+    <NuxtLink
+      :to="{
+        path: '/',
+      }"
+    >
+      <Button>Log In</Button>
+    </NuxtLink>
+  </template>
 </template>
